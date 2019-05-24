f8b14c11ef0ab5e0e7b488eb38082b981295374c
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-05-24 20:59:58 +0200

    ASP.NET core file structure

---
+++ chp2/BackAgain/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BackAgain.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
